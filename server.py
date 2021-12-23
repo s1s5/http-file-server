a11a4acc7d37f3a6e6816fa153fc85e10270eee3
Shogo Sawai <shogo.sawai@gmail.com>
2021-12-23 10:30:50 +0900

    bug fix, add delete

--- server.py
+++ server.py
@@ -8,9 +8,9 @@ from fastapi.responses import StreamingResponse
 
 app = FastAPI()
 
-DISKCACHE_DIRECTORY = os.environment.get('DISKCACHE_DIRECTORY', '/tmp/diskcache')
-DISKCACHE_SIZE_LIMIT = eval(os.environment.get('DISKCACHE_SIZE_LIMIT', '4 * (1024 **3)'))
-DISKCACHE_DEFAULT_EXPIRE = eval(os.environment.get('DISKCACHE_DEFAULT_EXPIRE', '24 * 60 * 60'))
+DISKCACHE_DIRECTORY = os.environ.get('DISKCACHE_DIRECTORY', '/tmp/diskcache')
+DISKCACHE_SIZE_LIMIT = eval(os.environ.get('DISKCACHE_SIZE_LIMIT', '4 * (1024 **3)'))
+DISKCACHE_DEFAULT_EXPIRE = eval(os.environ.get('DISKCACHE_DEFAULT_EXPIRE', '24 * 60 * 60'))
 
 cache = diskcache.Cache(directory=DISKCACHE_DIRECTORY, size_limit=DISKCACHE_SIZE_LIMIT)
 
@@ -35,3 +35,8 @@ async def upload_file(request: Request, filename: str):
         f.flush()
 
         await asyncio.to_thread(save_to_cache, f.name)
+
+
+@app.delete("/{filename}")
+def delete_file(filename: str):
+    cache.pop(filename)

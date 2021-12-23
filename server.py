26e7c4c25ea96ca2420f4edc079b3efb550949f2
Shogo Sawai <shogo.sawai@gmail.com>
2021-12-23 10:27:50 +0900

    use environment settings

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 
 import aiofiles
 import diskcache
@@ -6,8 +7,12 @@ from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import StreamingResponse
 
 app = FastAPI()
-cache = diskcache.Cache(directory='/tmp/diskcache', size_limit=4 * (1024 ** 3))
-DEFAULT_EXPIRE = 24 * 60 * 60
+
+DISKCACHE_DIRECTORY = os.environment.get('DISKCACHE_DIRECTORY', '/tmp/diskcache')
+DISKCACHE_SIZE_LIMIT = eval(os.environment.get('DISKCACHE_SIZE_LIMIT', '4 * (1024 **3)'))
+DISKCACHE_DEFAULT_EXPIRE = eval(os.environment.get('DISKCACHE_DEFAULT_EXPIRE', '24 * 60 * 60'))
+
+cache = diskcache.Cache(directory=DISKCACHE_DIRECTORY, size_limit=DISKCACHE_SIZE_LIMIT)
 
 
 @app.get("/{filename}")
@@ -22,7 +27,7 @@ def download_file(filename: str):
 async def upload_file(request: Request, filename: str):
 
     def save_to_cache(tmpfile):
-        cache.set(key=filename, value=open(tmpfile, 'rb'), expire=DEFAULT_EXPIRE, read=True)
+        cache.set(key=filename, value=open(tmpfile, 'rb'), expire=DISKCACHE_DEFAULT_EXPIRE, read=True)
 
     async with aiofiles.tempfile.NamedTemporaryFile('wb+') as f:
         async for chunk in request.stream():
